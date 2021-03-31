37c8010d76bda26ddd2805a6303bcdc914c3ed4a
agboolafullstack <81680926+agboolafullstack@users.noreply.github.com>
2021-03-31 10:42:39 +0100

    Create variables.tf

---
+++ infrastructure/rds/variables.tf
@@ -0,0 +1,14 @@
+variable "profile" {
+    description = "Profile to Auth to AWS"
+}
+
+variable "password" {
+    description = "password for db"
+}
+
+variable "id" {
+    description = "friendly name of instance in console"
+}
+variable "sec_id" {
+    description = "security group"
+}
